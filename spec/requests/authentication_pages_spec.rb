4f223a29deeac1ffcde969202f3a6c7ae8c38110
kuriyama_m <kuriyama@bbank.jp>
2014-10-10 22:50:32 +0900

    ADD USER FOLLOWING

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -102,6 +102,16 @@ describe "AuthenticationPages" do
         before { visit users_path }
         it { should have_title('Sign in') }
       end
+
+      describe "visiting the following page" do
+        before { visit following_user_path(user)}
+        it { should have_title('Sign in')}
+      end
+
+      describe "visiting the followers page" do
+        before {visit followers_user_path(user)}
+        it {should have_title('Sign in')}
+      end
     end
 
     describe "as wrong user" do
@@ -127,5 +137,21 @@ describe "AuthenticationPages" do
         before {delete user_path(user)}
       end
     end
+  end
+
+  describe "for non-signed-in users" do
+    let(:user) {FactoryGirl.create(:user)}
+
+     describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          # specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          # specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
   end
 end
--- spec/requests/user_page_spec.rb
+++ spec/requests/user_page_spec.rb
@@ -13,7 +13,7 @@ describe "User pages" do
 
     it { should have_title('All users')}
     it { should have_content('All users')}
-
+  
     describe "pagination" do
 
       before(:all){30.times{FactoryGirl.create(:user)}}
@@ -35,17 +35,18 @@ describe "User pages" do
       describe "as an admin user" do
         let(:admin){FactoryGirl.create(:admin)}
         before do
-          sign_in admin
-          visit users_path
-      end
+           sign_in admin
+           visit users_path
+        end
 
-      it {should have_link('delete', href: user_path(User.first))}
-      it "should be able to delete another user" do
-        expect do
-          click_link('delete', match: :first)
-        end.to change(User, :count).by(-1)
+        it {should have_link('delete', href: user_path(User.first))}
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it {should_not have_link('delete', href: user_path(admin))}
       end
-      it {should_not have_link('delete', href: user_path(admin))}
     end
   end
 
@@ -70,6 +71,46 @@ describe "User pages" do
       it { should have_content(m1.content)}
       it { should have_content(m2.content)}
       it { should have_content(user.microposts.count)}
+    end
+
+    describe "follow/unfollow buttons" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
+      describe "following a user" do
+        before { visit user_path(other_user) }
+
+        it "should increment the followed user count" do
+          expect do
+            click_button "Follow"
+          end.to change(user.followed_users, :count).by(1)
+        end
+
+        describe "toggling the button" do
+          before { click_button "Follow" }
+          it { should have_xpath("//input[@value='Unfollow']") }
+         end
+      end
+
+      describe "unfollowing a user" do
+        before do
+          user.follow!(other_user)
+          visit user_path(other_user)
+        end
+
+
+        it "should decrement the followed user count" do
+          expect do
+            click_button "Unfollow"
+          end.to change(other_user.followers, :count).by(-1)
+        end
+
+        describe "toggling the button" do
+          before {click_button "Unfollow"}
+          it { should have_xpath("//input[@value='Follow']") }
+        end
+      end
+
     end
   end
 
@@ -142,6 +183,31 @@ describe "User pages" do
         # specify {expect(user.reload.email).to eq new_email}
     end
   end
-end
 
+  describe "following/followers" do
+    let(:user) {FactoryGirl.create(:user)}
+    let(:other_user){FactoryGirl.create(:user)}
+    before { user.follow!(other_user)}
+
+    describe "followed users" do
+      before do
+        sign_in user
+        visit following_user_path(user)
+      end
+      it {should have_title(full_title('Following'))}
+      it {should have_selector('h3', text:'Following')}
+      it {should have_link(other_user.name, href: user_path(other_user))}
+    end
+
+    describe "followers" do
+      before do
+        sign_in other_user
+        visit followers_user_path(other_user)
+      end
+
+      it {should have_title(full_title('Followers'))}
+      it {should have_selector('h3', text: 'Followers')}
+      it {should have_link(user.name, href: user_path(user))}
+    end
+  end
 end
